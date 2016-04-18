096183de60b7959d3cbc5376a7a81ea96ac93e5a
Nils Eriksson <Nilseriksson89.ne@gmail.com>
2016-04-18 13:45:03 +0200

    writes tnx in input

--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,26 @@
 import $ from "jquery";
 
+
+var e = $.Event("keypress");
+e.which = 13; //choose the one you want
+e.keyCode = 13;
+
+const replies =
+  [ "Tack så mycket"
+  , "Tack"
+  , "Tack!, Ha en bra dag!"
+  ]
+
 $(window).on("scroll", () => {
   $('p:contains("Grattis")')
     .closest(".userContentWrapper")
     .each(function() {
-      console.log ($(this).find(".UFIComment").length)
+      const shouldSayTnx = $(this).find(".UFIComment").length === 0;
+      if(shouldSayTnx) {
+        console.log("say tnx man!");
+        const randomReply = replies[ Math.floor(Math.random() * replies.length) ];
+        $(this).find("._1mf._1mj span")
+              .html(`<span data-text="true">${randomReply}</span>`);
+      }
     });
-  // const commentField = arrayOfPotentialTnx.find(".UFIAddCommentInput");
-  // commentField.text("Herp");
 });
